fd74fa0b91bf82fb60b1888d8b8353e093558745
akira <asa.shakira340@gmail.com>
2022-12-17 14:26:44 +0900

    feat: range chart complete

--- src/features/HelloWorld/HelloWorld.tsx
+++ src/features/HelloWorld/HelloWorld.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import {RangeChart} from '../RangeChart'
+
+import styles from './HelloWorld.css'
+
 export const HelloWorld: React.FC = () => {
-  return <div>hi</div>
+  return (
+    <div className={styles.root}>
+      <RangeChart />
+    </div>
+  )
 }
--- src/features/RangeChart/RangeChart.tsx
+++ src/features/RangeChart/RangeChart.tsx
@@ -1,5 +1,52 @@
 import React from 'react'
 
+import styles from './RangeChart.css'
+
+type CardComboBoxProps = {
+  combo: string
+}
+
+const CardComboBox: React.FC<CardComboBoxProps> = ({combo}) => {
+  return <div className={styles.cardComboBox}>{combo}</div>
+}
+
 export const RangeChart: React.FC = () => {
-  return <div>RangeChart</div>
+  const cards = React.useMemo(() => ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2'], [])
+
+  const combos = React.useMemo(() => {
+    const combos = []
+    for (let i = 0; i < cards.length; i++) {
+      let suited = false
+      const row = []
+      for (let j = 0; j < cards.length; j++) {
+        let combo: string
+        if (cards[i] === cards[j]) {
+          // pocket pair
+          combo = cards[i] + cards[j]
+          suited = true
+        } else if (suited) {
+          // suited
+          combo = cards[i] + cards[j] + 's'
+        } else {
+          // off suit
+          combo = cards[j] + cards[i] + 'o'
+        }
+        row.push(combo)
+      }
+      combos.push(row)
+    }
+    return combos
+  }, [cards])
+
+  return (
+    <div className={styles.root}>
+      {combos.map((row: string[]) => (
+        <div key={row[0]} className={styles.cardComboRow}>
+          {row.map((combo: string) => (
+            <CardComboBox key={combo} combo={combo} />
+          ))}
+        </div>
+      ))}
+    </div>
+  )
 }
